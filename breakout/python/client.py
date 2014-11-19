051caa211074ce172e44edeebd14c2e6939a14ef
Finnian Anderson <finnian@fxapi.co.uk>
2014-11-19 12:39:14 +0000

    Reconnect on disconnect

--- breakout/python/client.py
+++ breakout/python/client.py
@@ -6,17 +6,21 @@ from socket import error as serror
 host = 'localhost'
 port = 42000
 
+
 def connect():
-	global irc
-	irc = socket(AF_INET, SOCK_STREAM)
-	irc.connect((host, port))
+    global irc
+    try:
+        irc = socket(AF_INET, SOCK_STREAM)
+        irc.connect((host, port))
+    except serror:
+        print("Server offline :(")
 
 connect()
 while True:
-	try:
-		data = irc.recv(4096)
-		irc.send('Hi! I am client %s.' % data)
-		print("I am client %s as told by server" % data)
-	except serror:
-		print ("Server offline :(")
-	time.sleep(5)
+    try:
+        data = irc.recv(4096)
+        irc.send('Hi! I am client %s.' % data)
+        print("I am client %s as told by server" % data)
+    except serror:
+        connect()
+    time.sleep(5)

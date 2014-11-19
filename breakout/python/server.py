94bfe537472a349a8d4d2b8474a74f5cf6bee5d9
Finnian Anderson <finnian@fxapi.co.uk>
2014-11-19 12:39:30 +0000

    Fix server.py

--- breakout/python/server.py
+++ breakout/python/server.py
@@ -5,28 +5,33 @@ from socket import error as serror
 
 host = ''
 port = 42000
- 
+
 sock = socket()
 sock.bind((host, port))
 sock.listen(5)
 
 clients = list()
 
+
 def clientthread(conn, id):
-	while True:
-		try:
-			conn.send(str(id))
-			data = conn.recv(1024)
-			print(data)
-		except serror:
-			print("Client %s left" % id)
-			conn.close()
+    while True:
+        try:
+            conn.send(str(id))
+            data = conn.recv(1024)
+            print("Client %s: %s" % (str(id), str(data)))
+        except serror:
+            print("Client %s: left" % id)
+            conn.close()
+            break
 
 while True:
-    conn, addr = sock.accept()
-    clients.append(addr)
-    print("Client %i just joined!" % clients.index(addr))
-    start_new_thread(clientthread,(conn,clients.index(addr)))
-
-conn.close()
-sock.close()
+    print("<<<<<<<<<<<<<<<<<<<< No of clients: %s >>>>>>>>>>>>>>>>>>>>" %
+          len(clients))
+    try:
+        conn, addr = sock.accept()
+        clients.append(addr)
+        print("Client %i: joined!" % clients.index(addr))
+        start_new_thread(clientthread, (conn, clients.index(addr)))
+    except KeyboardInterrupt:
+        conn.close()
+        sock.close()
